f532780865c3e54c95876071c2770b64c52aabb5
zullum <zullumsan@gmail.com>
2021-11-23 10:43:57 +0100

    Add folders and files structure.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  Footer,
+  Header,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Blog,
+} from "./containers";
+
+import { Cta, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
